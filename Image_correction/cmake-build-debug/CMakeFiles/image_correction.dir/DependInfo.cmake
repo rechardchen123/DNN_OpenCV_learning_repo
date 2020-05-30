f5860491617b894b9ef4516bcacbbf1e7b667b9d
rechardchen123 <rechardchen123@outlook.com>
2020-05-30 20:32:06 +0100

    image correction

---
+++ Image_correction/cmake-build-debug/CMakeFiles/image_correction.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/ucesxc0/Documents/Repository-My programming and coding/DNN_OpenCV_learning_repo/Image_correction/main.cpp" "C:/Users/ucesxc0/Documents/Repository-My programming and coding/DNN_OpenCV_learning_repo/Image_correction/cmake-build-debug/CMakeFiles/image_correction.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/minGW-build"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/core/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/flann/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/imgproc/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/ml/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/phase_unwrapping/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/photo/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/plot/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/quality/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/reg/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/surface_matching/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/xphoto/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/dnn/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/features2d/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/fuzzy/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/gapi/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/hfs/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/img_hash/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/imgcodecs/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/line_descriptor/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/saliency/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/text/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/videoio/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/calib3d/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/datasets/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/dnn_superres/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/highgui/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/objdetect/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/rgbd/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/shape/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/structured_light/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/ts/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/video/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/videostab/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/xfeatures2d/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/ximgproc/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/xobjdetect/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/aruco/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/bgsegm/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/bioinspired/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/ccalib/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/dnn_objdetect/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/dpm/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/face/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/optflow/include"
+  "C:/Users/ucesxc0/Downloads/opencv-4.2.0/modules/stitching/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/superres/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/tracking/include"
+  "C:/Users/ucesxc0/Downloads/opencv_contrib-4.2.0/modules/stereo/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
